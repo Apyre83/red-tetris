3e1fae10c2e93a4b9aff2e56eb17fce27dca121b
Clemence <clemence.l@somanity.com>
2024-02-21 15:19:20 +0100

    add tests for CREATE_GAME and JOIN_GAME

--- backend/src/Classes/Server.test.js
+++ backend/src/Classes/Server.test.js
@@ -2,6 +2,7 @@ const Server = require('./Server');
 const ioClient = require('socket.io-client');
 const Player = require('./Player');
 const e = require('express');
+const Game = require('./Game');
 
 describe('Server', () => {
     let server;
@@ -94,5 +95,108 @@ describe('Server', () => {
         });
 
     });
+
+    test('should successfully register a new user', (done) => {
+        jest.spyOn(server, 'readDatabase').mockReturnValue({});
+        const writeDbSpy = jest.spyOn(server, 'writeDatabase').mockImplementation(() => {});
+
+        clientSocket.emit('SIGNUP', { username: 'newUser', password: 'newPassword', email: 'newEmail@example.com' }, (response) => {
+            expect(response.code).toBe(0); 
+            expect(writeDbSpy).toHaveBeenCalled();
+            done();
+        });
+    });
+
+    test('should fail if the username already exists', (done) => {
+        jest.spyOn(server, 'readDatabase').mockReturnValue({
+            'newUser': { password: 'newPassword', email: 'newEmail@example.com' }
+        });
+
+        clientSocket.emit('SIGNUP', { username: 'newUser', password: 'newPassword', email: 'anotherEmail@example.com' }, (response) => {
+            expect(response.code).toBe(1); 
+            done();
+        });
+    });
+
+    test('GET_SCORE event should return the correct score for an existing player', (done) => {
+        const testPlayerName = 'existingUser';
+        const testScore = 100;
+    
+        jest.spyOn(server, 'readDatabase').mockReturnValue({
+            [testPlayerName]: { allTimeScores: testScore }
+        });
+    
+        clientSocket.emit('GET_SCORE', { playerName: testPlayerName }, response => {
+            expect(response.code).toBe(0);
+            expect(response.score).toBe(testScore);
+            done();
+        });
+    });
+
+    test('CREATE_GAME event should create a new game', (done) => {
+        const gameName = 'testGame';
+        clientSocket.emit('CREATE_GAME', { gameName }, (response) => {
+            expect(response.code).toBe(0);
+            done();
+        })
+    });
+
+    test('CREATE_GAME event should return errors', (done) => {
+        const newGame = new Game(server, 'testGame');
+        const gameCreator = 'creatorName';
+        const newPlayer = new Player('', gameCreator, '');
+
+    
+        server.games.push(newGame);
+        newGame.addPlayer(newPlayer, () => {});
+
+        const gameName = 'testGame';
+
+        clientSocket.emit('CREATE_GAME', { gameName: gameName }, (response) => {
+            expect(response.code).toBe(1);
+        })
+
+        clientSocket.emit('CREATE_GAME', { gameName: 'validGameName', playerName: gameCreator }, (response) => {
+            expect(response.code).toBe(2);
+            done();
+        })
+
+    });
+
+    test('JOIN_GAME event should let a player join an existing game', done => {
+        const gameName = 'TestGame';
+        const playerName = 'TestPlayer';
+        const playerSocketId = clientSocket.id;
+    
+        server.games.push(new Game(server, gameName));
+        server.players.push(new Player({id: playerSocketId}, playerName, {}));
+    
+        clientSocket.emit('JOIN_GAME', { gameName: gameName, playerName: playerName }, (response) => {
+            expect(response.code).toBe(0);
+            const game = server.games.find(g => g.gameName === gameName);
+            expect(game.players.some(p => p.playerName === playerName)).toBe(true);
+            done();
+        });
+    });
+    
+    test('JOIN_GAME event should not allow joining a running game', done => {
+        const gameName = 'RunningGame';
+        const playerName = 'TestPlayer';
+        const playerSocketId = clientSocket.id;
+    
+        const newGame = new Game(server, gameName);
+        server.games.push(newGame);
+    
+        newGame.gameIsRunning = true;
+    
+        server.players.push(new Player({id: playerSocketId}, playerName, {}));
+    
+        clientSocket.emit('JOIN_GAME', { gameName: gameName, playerName: playerName }, response => {
+            expect(response.code).toBe(4);
+            done();
+        });
+    });
+    
+
     
 });
